050af174133d0d28e7d0fff7de5a5e834a7fc3dc
clownmonster <mk6386223@gmail.com>
2021-05-08 16:34:37 +0530

    symptom checker done

--- medcare/Home/urls.py
+++ medcare/Home/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import landingpage, login_view, register, home, bookAppointment
+from .views import landingpage, login_view, register, home, bookAppointment, symptomChecker
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('clienthome/',home,name='clienthome'),
     path('bookappointment/',bookAppointment, name='bookappointment'),
+    path('symptomchecker/',symptomChecker,name='symptomchecker'),
 ]
--- medcare/Home/views.py
+++ medcare/Home/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from .models import Doctors
+import requests
 
 
 # Create your views here.
@@ -78,3 +79,34 @@ def bookAppointment(request):
         print(val)
     queryset = Doctors.objects.all()
     return render(request,'bookappointment.html',{'queryset':queryset})
+
+#################################################################################
+
+def symptomChecker(request):
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjFtczE4Y3MwODZAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI5MDY1IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTA1LTA3IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MjA0NzU1OTUsIm5iZiI6MTYyMDQ2ODM5NX0.1GHFzRPimQjLEINNHiLcWngrShzqelTSp440uU_z4LM" 
+    #token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjFtczE4Y3MwODZAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI5MDY1IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTA1LTA3IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MjA0Njc5NDMsIm5iZiI6MTYyMDQ2MDc0M30.-yDPoaWH8BjmHX0FAfnb-zdtYySPXmPmm70aiy_Hm0s"
+    if request.method == 'POST':
+        val = request.POST.get('age')
+        gender = request.POST.get('gender')
+        res = requests.request("GET", f'https://sandbox-healthservice.priaid.ch/symptoms?token={token}&language=en-gb&format=json')
+        data = res.json()
+        symptoms = [[]]
+        for d in data:
+            symptoms.append([d['ID'],d['Name']])
+        del(symptoms[0])
+        return render(request, 'symptomchecker.html',{
+        'age':False,
+        'symptom':True,
+        'diagnosis':False,
+        'issue':False,
+        'symptoms':symptoms
+
+    })
+    
+   
+    return render(request,'symptomchecker.html', {
+        'age':True,
+        'symptom':True,
+        'diagnosis':False,
+        'issue':False,
+    })
