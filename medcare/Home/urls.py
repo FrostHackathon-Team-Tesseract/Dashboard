920a95aac919496630e441156ddae46f0f28e63c
clownmonster <mk6386223@gmail.com>
2021-05-08 09:29:34 +0530

    book appointment setup done

--- medcare/Home/urls.py
+++ medcare/Home/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import landingpage, login_view, register, home, map_view
+from .views import landingpage, login_view, register, home, bookAppointment
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     path('registerclient/',register,name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('clienthome/',home,name='clienthome'),
-    path('bookappointment/')
+    path('bookappointment/',bookAppointment, name='bookappointment'),
 ]
