8e250433aeee86ea63e26e681b1bd2349ab003e0
clownmonster <mk6386223@gmail.com>
2021-05-08 19:36:22 +0530

    map is realtime now

--- medcare/Home/views.py
+++ medcare/Home/views.py
@@ -69,7 +69,18 @@ def register(request):
     
 
 def home(request):
-    return render(request,'home.html')
+    response = requests.get('https://api.rootnet.in/covid19-in/stats/latest')
+    print(response.status_code)
+    data = (response.json())
+    data = data['data']['regional']
+    statewise_data = {}
+
+    for i in data:
+        s = {
+        i['loc']:i['confirmedCasesIndian']     
+        }
+        statewise_data.update(s)
+    return render(request,'home.html',{'statewise_data':statewise_data})
 
 
 
--- medcare/test.py
+++ medcare/test.py
@@ -1,5 +1,15 @@
 import requests
 
-response = requests.get('https://api.endlessmedical.com/v1/dx/GetFeatures')
+response = requests.get('https://api.rootnet.in/covid19-in/stats/latest')
 print(response.status_code)
-print(response.json())
+data = (response.json())
+data = data['data']['regional']
+
+statewise_data = {}
+
+for i in data:
+    s = {
+    i['loc']:i['confirmedCasesIndian']     
+    }
+    statewise_data.update(s)
+print(statewise_data)
